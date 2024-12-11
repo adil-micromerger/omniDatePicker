42339d1e0793d7cb0493dae2ec37c09e8d66900a
adil-micromerger <132429805+adil-micromerger@users.noreply.github.com>
2024-12-11 16:16:22 +0500

    change: button style

--- lib/src/components/button_row.dart
+++ lib/src/components/button_row.dart
@@ -15,35 +15,147 @@ class ButtonRow extends StatelessWidget {
       children: [
         Expanded(
           flex: 5,
-          child: Padding(padding: EdgeInsets.only(left: 20),child: OutlinedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            style: OutlinedButton.styleFrom(
-              side:BorderSide(color: Color(0xffE5E8EB)) ,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
-              ),
+          child: Padding(padding: EdgeInsets.only(left: 20),
+            child: BButton(
               backgroundColor: Colors.white,
-            ),
-            child: Text('Cancel', style: TextStyle(color: Color(0xff2E528D),fontWeight: FontWeight.w700,fontSize: 16),),
-          ),),
+              borderColor: Color(0xffE5E8EB) ,
+              borderRadius: 4,
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel', style: TextStyle(
+                  color: Color(0xff2E528D),
+                  fontWeight: FontWeight.w700,
+                  fontSize: 16
+              ),),
+            ),),
         ),
         SizedBox(width: 15,),
         Expanded(
-          flex: 7,
-            child: Padding(padding: EdgeInsets.only(right: 20),child: OutlinedButton(
-              onPressed: onSavePressed,
-              style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                backgroundColor: Color(0xff2E528D),
-              ),
-              child: Text('Save', style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700,fontSize: 16)),
-            ),)
+            flex: 7,
+            child: Padding(padding: EdgeInsets.only(right: 20),
+              child: CButton(
+                onTap: onSavePressed,
+                buttonColor: Color(0xff2E528D),
+                child: Text('Save', style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16)),
+              ),)
         ),
       ],
     );
   }
 }
+
+class CButton extends StatelessWidget {
+  Widget child;
+  Function? onTap;
+  double? verticalPadding;
+  double? horizontalPadding;
+  Color? buttonColor;
+  bool? isLoading;
+  Color? textColor;
+  String? iconPath;
+  bool? isIconButton = false;
+  CButton({
+    super.key,
+    this.isLoading,
+    required this.child,
+    required this.onTap,
+    this.horizontalPadding,
+    this.verticalPadding,
+    this.buttonColor,
+    this.textColor,
+    this.iconPath,
+    this.isIconButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: () {
+              if (onTap == null) {
+              } else {
+                onTap!();
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: buttonColor!,
+              shadowColor: Colors.transparent,
+              elevation: 0.0,
+              splashFactory: NoSplash.splashFactory,
+              padding: EdgeInsets.symmetric(
+                  horizontal: horizontalPadding ?? 17,
+                  vertical: verticalPadding ?? 16),
+              textStyle: TextStyle(
+                  color: textColor ?? Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 16),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                child,
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+class BButton extends StatelessWidget {
+  Widget child;
+  Function? onTap;
+  final Color? backgroundColor;
+  final Color? borderColor;
+  final double? borderRadius;
+  double? verticalPadding;
+  double? horizontalPadding;
+
+  BButton({
+    super.key,
+    required this.child,
+    required this.onTap,
+    this.backgroundColor,
+    this.borderColor = Colors.white,
+    this.borderRadius = 0,
+    this.horizontalPadding,
+    this.verticalPadding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(
+              horizontal: horizontalPadding ?? 17, vertical: verticalPadding ?? 14),
+          shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: borderColor!,
+              ),
+              borderRadius: BorderRadius.circular(
+                borderRadius!,
+              )),
+          backgroundColor: backgroundColor,
+          surfaceTintColor: Colors.transparent),
+      onPressed: () {
+        if (onTap == null) {
+        } else {
+          onTap!();
+        }
+      },
+      child: child,
+    );
+  }
+}
+
